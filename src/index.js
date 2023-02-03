d7ba065b4c1332cd8d6a22a83312aed44abf511e
Viktar <viktarbrutsis@gmail.com>
2023-02-03 12:53:18 +0300

    feat: 18/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  if (str.length % 2 !== 0) {
+    return false;
+  } else {
+    for (let i = 0; i < str.length; i ++) {
+      if (str[i] == '(' || str[i] == '{' || str[i] == '[') {
+        stack.push(str[i]);
+        } if (str[i] == '|' && stack[stack.length - 1] !== '|') {
+          stack.push(str[i]);
+        } 
+        else if (str[i] == ')' && stack[stack.length - 1] == '(' || str[i] == '}' && stack[stack.length - 1] == '{' || str[i] == ']' && stack[stack.length - 1] == '[' || str[i] == '|' && stack[stack.length - 1] == '|') {
+          stack.pop();
+        } else if (str[i] == ')' && stack[stack.length - 1] !== '(' || str[i] == '}' && stack[stack.length - 1] !== '{' || str[i] == ']' && stack[stack.length - 1] !== '[') {
+          stack.push(str[i]);
+        }
+    }
+    if (stack.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+};
+
+//const string = '((((((((((((())))))))))))))()';
+
+//const stack = [];
+
+//function check(str) {
+//  let stack = [];
+//  for (let i = 0; i < str.length; i ++) {
+//    if (str[i] == '(' || str[i] == '{' || str[i] == '[') {
+//      stack.push(str[i]);
+//      } if (str[i] == '|' && stack[stack.length - 1] !== '|') {
+//        stack.push(str[i]);
+//      } 
+//      else if (str[i] == ')' && stack[stack.length - 1] == '(' || str[i] == '}' && stack[stack.length - 1] == '{' || str[i] == ']' && stack[stack.length - 1] == '[' || str[i] == '|' && stack[stack.length - 1] == '|') {
+//        stack.pop();
+//      } else if (str[i] == ')' && stack[stack.length - 1] !== '(' || str[i] == '}' && stack[stack.length - 1] !== '{' || str[i] == ']' && stack[stack.length - 1] !== '[') {
+//        stack.push(str[i]);
+//      }
+//  }
+
+//  console.log(stack);
+//}
+
+
+//check(string);
+
